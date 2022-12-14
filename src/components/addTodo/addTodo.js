fdcd3281a9b7e83afc2fccfdc847c8cec2d82354
villalb4 <villalb4angel@gmail.com>
2022-12-13 22:18:05 -0300

    get localStorage items

--- src/components/addTodo/addTodo.js
+++ src/components/addTodo/addTodo.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './addTodo.css'
 
 function AddTodo() {
+
+  const [item, setItem] = useState({
+    id: crypto.randomUUID(),
+    task: "",
+    completed: false,
+  })
+
+  const handleChange = (e) => {
+    const tarea = e.target.value
+    setItem({task : tarea})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    localStorage.setItem('tasks', JSON.stringify(item))
+    setItem({id: "", task: "", completed: false})
+  }
+
   return (
     <div className='addTodo_component'>
       <div className='addTodo_divContainer'>
-        <input className='addTodo_input' placeholder='add details'/>
-        <button className='addTodo_button'>Add</button>
+        <input className='addTodo_input' placeholder='add details' value={item.task} onChange={handleChange}/>
+        <input type="submit" className='addTodo_button' value="add" onClick={handleSubmit}/>
       </div>
     </div>
   )
