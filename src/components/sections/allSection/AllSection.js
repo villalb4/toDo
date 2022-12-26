881e2be58c9034880dba894a16e197ef61aa57e5
villalb4 <villalb4angel@gmail.com>
2022-12-26 17:32:20 -0300

    add: get localStorage item & map

--- src/components/sections/allSection/AllSection.js
+++ src/components/sections/allSection/AllSection.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './AllSection.css'
 
 function AllSection() {
+
+  const getItems = JSON.parse(localStorage.getItem('todoList'))
+
   return (
     <div>
-      All
+      <div className="allSection_divContent">
+        {getItems !== null ? getItems.map((e,i) => {
+          return(
+            <div key={i} className="allSection_divInput">
+              <input type={'checkbox'} className="allSection_checkbox"/>
+              <p className='allSection_pTask'>{e.task}</p>
+            </div>
+          )
+        }) : null}
+      </div>
     </div>
   )
 }
