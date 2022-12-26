587ac0c100b55ddf2822b1e5c9c7a4b21f14c815
villalb4 <villalb4angel@gmail.com>
2022-12-26 17:32:49 -0300

    add: new slice

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navegation from "../slice/navegation";
+import tasks from "../slice/tasks";
 
 export const store = configureStore({
   reducer: {
     navegation,
+    tasks
   },
 })
