45972fb933d79d2474a700d6ece25a2c140f985b
villalb4 <villalb4angel@gmail.com>
2022-12-30 19:27:09 -0300

    change: refactor

--- src/components/sections/allSection/AllSection.js
+++ src/components/sections/allSection/AllSection.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 import './AllSection.css'
 
 function AllSection() {
 
-  const getItems = JSON.parse(localStorage.getItem('todoList'))
+  const getItems = useSelector(e => e.tasks.localTask)
 
   return (
     <div>
       <div className="allSection_divContent">
-        {getItems !== null ? getItems.map((e,i) => {
+        {getItems.map((e,i) => {
           return(
             <div key={i} className="allSection_divInput">
               <input type={'checkbox'} className="allSection_checkbox"/>
               <p className='allSection_pTask'>{e.task}</p>
             </div>
           )
-        }) : null}
+        })}
       </div>
     </div>
   )
