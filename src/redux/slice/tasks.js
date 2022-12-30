69adf2b259fb88aabebb89a35b7af72562073720
villalb4 <villalb4angel@gmail.com>
2022-12-30 19:23:51 -0300

    add: new toDo add working

--- src/redux/slice/tasks.js
+++ src/redux/slice/tasks.js
@@ -1,24 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const localStorageTask = localStorage.getItem('todoList') !== null
-  ? localStorage.getItem('todoList') : []
+  ? JSON.parse(localStorage.getItem('todoList')) 
+  : []
 
 const initialState = {
-  localTareas: localStorageTask,
-  tareas : []
+  localTask: localStorageTask,
 }
 
 const taskSlice = createSlice({
   name: "tasks",
   initialState: initialState,
   reducers : {
-    setTask: function(state, action) { 
-      state.tareas = [...state.tareas, action.payload]
-      localStorage.setItem('todoList', JSON.stringify(state.tareas.map(e => e)))
+    setTask: function(state, action) {
+      state.localTask.push(action.payload)
+      const toDo = JSON.parse(localStorage.getItem('todoList'))
+      if(toDo) {
+        toDo.push({...action.payload})
+        localStorage.setItem('todoList', JSON.stringify(toDo))
+      } 
+      else {
+        localStorage.setItem('todoList', JSON.stringify([{...action.payload}]))
+      }
     }
   }
 })
 
-export const {setLocalStorage, setTask} = taskSlice.actions
+export const { setTask } = taskSlice.actions
 export default taskSlice.reducer
 
