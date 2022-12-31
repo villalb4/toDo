3c4113abd21415050d7c93eaaf383acf00a51cae
villalb4 <villalb4angel@gmail.com>
2022-12-31 00:54:12 -0300

    add: AddTodo component

--- src/components/sections/allSection/AllSection.js
+++ src/components/sections/allSection/AllSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AddTodo from '../../addTodo/addTodo'
 import { useSelector } from 'react-redux'
 
 import './AllSection.css'
@@ -9,6 +10,7 @@ function AllSection() {
 
   return (
     <div>
+      <AddTodo />
       <div className="allSection_divContent">
         {getItems.map((e,i) => {
           return(
