c38bb16166a93bcb1a3e21e1eea303740878069a
villalb4 <villalb4angel@gmail.com>
2022-12-31 00:53:35 -0300

    add: render active tasks

--- src/components/sections/activeSection/ActiveSection.js
+++ src/components/sections/activeSection/ActiveSection.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import AddTodo from '../../addTodo/addTodo'
+import { useSelector } from 'react-redux'
 
 function ActiveSection() {
+
+  const getItems = useSelector(e => e.tasks.localTask)
+  const itemsFiltered = getItems.filter(e => e.completed === false)
+
   return (
     <div>
-      Active
+      <AddTodo />
+      <div className="allSection_divContent">
+        {itemsFiltered.map((e,i) => {
+          return(
+            <div key={i} className="allSection_divInput">
+              <input type={'checkbox'} className="allSection_checkbox"/>
+              <p className='allSection_pTask'>{e.task}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
